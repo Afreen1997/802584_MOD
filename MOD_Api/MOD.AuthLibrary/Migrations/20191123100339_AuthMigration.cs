f95031c2c07cfd103127850f0762527529db8144
HP <HP@DESKTOP-T242PDF>
2019-11-25 17:22:26 +0530

    new

---
+++ MOD_Api/MOD.AuthLibrary/Migrations/20191123100339_AuthMigration.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MOD.AuthLibrary.Migrations
+{
+    public partial class AuthMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Prerequisites",
+                table: "Skills");
+
+            migrationBuilder.DropColumn(
+                name: "TOC",
+                table: "Skills");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "1",
+                column: "ConcurrencyStamp",
+                value: "09a39829-170a-4bc0-9748-2bd44db78958");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "2",
+                column: "ConcurrencyStamp",
+                value: "fbfcf436-06c3-48e2-87f8-9e34cefa1be2");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "3",
+                column: "ConcurrencyStamp",
+                value: "be950136-e8a3-4007-bc51-a0f3069f1b78");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Prerequisites",
+                table: "Skills",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "TOC",
+                table: "Skills",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "1",
+                column: "ConcurrencyStamp",
+                value: "d5e49ccb-6439-4156-9815-f9545f2888f4");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "2",
+                column: "ConcurrencyStamp",
+                value: "135f1915-5ba8-4c40-9ba9-bb4abb96bfdd");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "3",
+                column: "ConcurrencyStamp",
+                value: "c80f5f3c-2253-4fe4-842c-4900d928e62b");
+        }
+    }
+}
---
+++ MOD_Api/MOD.ModelLibrary/Skill.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MOD.ModelLibrary
+{
+    public class Skill
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+      
+        public int Fees { get; set; }
+        
+    }
+}
